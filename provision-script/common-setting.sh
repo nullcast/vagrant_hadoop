9710aa8340c5447e9459e4aaa99aef825e64b310
M.kurosawa <yonama.dev@gmail.com>
2018-01-13 23:49:38 +0900

    provisionの修正

--- provision-script/common-setting.sh
+++ provision-script/common-setting.sh
@@ -10,21 +10,6 @@ tail -n 3 /etc/hosts
 sudo yum -y install epel-release
 sudo yum -y install openssh-clients rsync wget java-1.8.0-openjdk-devel sshpass
 
-# .bashrc
-cat << 'EOF' >> /home/vagrant/.bashrc
-export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk
-export HADOOP_HOME=/home/vagrant/hadoop-2.8.2
-export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
-export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$JAVA_HOME/bin:$PATH
-EOF
-cat << 'EOF' >> /root/.bashrc
-export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk
-export HADOOP_HOME=/home/vagrant/hadoop-2.8.2
-export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
-export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$JAVA_HOME/bin:$PATH
-EOF
-source /home/vagrant/.bashrc
-
 # ssh
 cat /vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 mkdir /root/.ssh
@@ -34,3 +19,51 @@ chmod 600 /root/.ssh/authorized_keys
 cp /vagrant/.ssh/id_rsa /home/vagrant/.ssh/id_rsa
 chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
 cp /vagrant/.ssh/id_rsa /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+chmod 600 /home/vagrant/.ssh/id_rsa
+cat << 'EOF' >> /etc/ssh/ssh_config
+    StrictHostKeyChecking no
+EOF
+
+# pyenv
+yum -y install git gcc zlib-devel bzip2 bzip2-devel readline readline-devel sqlite sqlite-devel openssl openssl-devel
+export PYENV_ROOT=/home/vagrant/.pyenv
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+chmod 777 -R $PYENV_ROOT
+
+# .bashrc
+cat << 'EOF' >> /home/vagrant/.bashrc
+# JAVA
+export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk
+
+# HADOOP
+export HADOOP_HOME=/home/vagrant/hadoop-2.8.2
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$JAVA_HOME/bin:$PATH
+
+# PYENV
+export PYENV_ROOT=/home/vagrant/.pyenv
+export PATH=$PYENV_ROOT/bin:$PATH
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+EOF
+cat << 'EOF' >> /root/.bashrc
+# JAVA
+export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk
+
+# HADOOP
+export HADOOP_HOME=/home/vagrant/hadoop-2.8.2
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$JAVA_HOME/bin:$PATH
+
+# PYENV
+export PYENV_ROOT=/home/vagrant/.pyenv
+export PATH=$PYENV_ROOT/bin:$PATH
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+EOF
+source /home/vagrant/.bashrc
+
+# python
+pyenv install 3.6.2
+pyenv global 3.6.2
